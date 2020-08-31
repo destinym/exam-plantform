ab63d6a34d1f4e776d87370e13210f43b59fbba3
mengliang <mengliang1225@aliyun.com>
2020-08-31 22:59:54 +0800

    delete unused file

--- src/main/java/exam/paperContent/domain/model/paper/Paper.java
+++ src/main/java/exam/paperContent/domain/model/paper/Paper.java
@@ -3,14 +3,10 @@ package exam.paperContent.domain.model.paper;
 import exam.Entity;
 import exam.paperContent.domain.model.quiz.BlankQuiz;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
-@Slf4j
 @Getter
 public class Paper implements Entity<Paper> {
     private PaperId paperId;
